9699147ce0ab251f577b937c4b431f94921c5082
JohanMolina-dev <jsmolina49@gmail.com>
2021-05-07 17:47:25 -0500

    asserts

--- src/main/java/MyStorePages/Login.java
+++ src/main/java/MyStorePages/Login.java
@@ -8,6 +8,9 @@ public class Login {
     private By inputEmail = By.cssSelector("input#email");
     private By inputPassword = By.cssSelector("input#passwd");
     private By buttonSignIn = By.id("SubmitLogin");
+    private By nameUser =  By.cssSelector("header#header nav > div:nth-child(1) > a > span");
+    private By contentH1page = By.cssSelector("div#center_column > h1");
+    private By contentAlert = By.cssSelector("div#center_column li");
 
     public Login(WebDriver driver) {
         this.driver = driver;
@@ -18,8 +21,18 @@ public class Login {
     public void userPassword(String password){
         driver.findElement(inputPassword).sendKeys(password);
     }
-    public void clickButtonSignIn(){
+    public MyPersonalInfo clickButtonSignIn(){
         driver.findElement(buttonSignIn).click();
+        return new MyPersonalInfo(driver);
+    }
+    public String getNameUser(){
+        return driver.findElement(nameUser).getText();
+    }
+    public String getNamePage(){
+        return driver.findElement(contentH1page).getText();
+    }
+    public String getTextAlert(){
+        return driver.findElement(contentAlert).getText();
     }
 
 
---
+++ src/main/java/MyStorePages/MyPersonalInfo.java
@@ -0,0 +1,34 @@
+package MyStorePages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import java.util.Locale;
+
+public class MyPersonalInfo {
+    private WebDriver driver;
+    private By buttonMyInfo = By.cssSelector("div#center_column li:nth-child(4) > a > span");
+    private By nameInfo = By.cssSelector("input#firstname");
+    private By lastNameInfo = By.cssSelector("input#lastname");
+    private By saveMyChange = By.cssSelector("div#center_column button[type=\"submit\"] > span");
+    private By alertText = By.cssSelector("div#center_column ol > li");
+
+    public MyPersonalInfo(WebDriver driver) {
+        this.driver = driver;
+    }
+    public void clickButton(){
+        driver.findElement(buttonMyInfo).click();
+    }
+    public void changeMyName(String newName){
+        driver.findElement(nameInfo).sendKeys(newName.toLowerCase());
+    }
+    public void changeMyLastName(String newLastName){
+        driver.findElement(lastNameInfo).sendKeys(newLastName.toLowerCase());
+    }
+    public void clickSave(){
+        driver.findElement(saveMyChange).click();
+    }
+    public String getMyAlert(){
+        return driver.findElement(alertText).getText();
+    }
+}
